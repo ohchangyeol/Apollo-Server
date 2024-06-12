13c678e0a461cfbf0432df3d80333300f10d87fb
changyeol oh <cy950315@naver.com>
2024-06-12 18:25:46 +0900

    mybatis 연결

--- database/index.js
+++ database/index.js
@@ -1,4 +1,8 @@
-const conn = mysql.createPool();
+import mysql from 'mysql2/promise';
+import createMapper from "./mapper/index.js";
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const dbConfig = {
     host : process.env.DB_HOST,
@@ -12,3 +16,57 @@ const dbConfig = {
     connectionLimit: process.env.MAX_CONNECTION,
     maxIdle: process.env.MAX_IDLE,
 }
+
+function getConnection() {
+    const pool = mysql.createPool(dbConfig);
+    createMapper; 
+    return pool.getConnection();
+}
+
+export async function queryRead(query) {
+    const client = await getConnection();
+
+    // console.log(client);
+
+    const [result] = await client.query(query);
+
+    try {
+        if(Array.isArray(result)){
+            return {error: false, rows: result, rowCount: result.length};
+        }else{
+            return {error: false, rows: [result], rowCount: result.affectedRows};
+        }
+    } catch (error) {
+        await client.rollback(); // 에러 발생 시 트랜잭션 롤백
+        console.error('Database 쿼리 오류 발생:', error.message);
+        // 에러 리턴하도록 errorResult 생성
+        return {error: true, rows: [], rowCount: 0};
+    } finally {
+        client.release();
+    }
+}
+
+export async function queryWrite(query) {
+    const client = await getConnection();
+    await client.beginTransaction(); // 트랜잭션 시작
+
+    const [result] = await client.query(query);
+
+    try {
+        if(Array.isArray(result)){
+            await client.commit(); // 트랜잭션 커밋
+            return {error: false, rows: result, rowCount: result.length};
+        }else{
+            await client.commit(); // 트랜잭션 커밋
+            return {error: false, rows: [result], rowCount: result.affectedRows};
+        }
+    } catch (error) {
+        await client.rollback(); // 에러 발생 시 트랜잭션 롤백
+        console.error('Database 쿼리 오류 발생:', error.message);
+        // 에러 리턴하도록 errorResult 생성
+        return {error: true, rows: [], rowCount: 0};
+    } finally {
+        client.release();
+    }
+}
+
--- test/dbtester.js
+++ test/dbtester.js
@@ -5,43 +5,47 @@
 
 // import conn from "../config/maria.js";
 
-import mysql from 'mysql2/promise';
-import dotenv from 'dotenv'
-import createMapper from "../database/mapper/index.js";
+// import mysql from 'mysql2/promise';
+// import dotenv from 'dotenv'
+// import createMapper from "../database/mapper/index.js";
 import mybatisMapper from 'mybatis-mapper';
+import {queryRead} from "../database/index.js"
 
-dotenv.config();
+// dotenv.config();
 
-const conn = mysql.createPool({
-    host : process.env.DB_HOST,
-    port : process.env.DB_PORT,
-    user:process.env.DB_USER,
-    password:process.env.DB_PWD,
-    database: process.env.DB_NAME,
-    connectTimeout: 3000,
-    connectionLimit: 30 ,//default 10
-    waitForConnections: true,
-    connectionLimit: process.env.MAX_CONNECTION,
-    maxIdle: process.env.MAX_IDLE,
-});
+// const conn = mysql.createPool({
+//     host : process.env.DB_HOST,
+//     port : process.env.DB_PORT,
+//     user:process.env.DB_USER,
+//     password:process.env.DB_PWD,
+//     database: process.env.DB_NAME,
+//     connectTimeout: 3000,
+//     connectionLimit: 30 ,//default 10
+//     waitForConnections: true,
+//     connectionLimit: process.env.MAX_CONNECTION,
+//     maxIdle: process.env.MAX_IDLE,
+// });
 
 // console.log(__dirname);
 
-createMapper
+// createMapper
+
 
 
 const query = mybatisMapper.getStatement('boardMapper','getBoardList', { language: 'sql', indent: '  ' } );
 
 console.log(query);
 
-const test = async()=>{
-    const connnn = await conn.getConnection();
-    const [res] = await connnn.query(query);
-    connnn.release();
-    return res;
-}
+// const test = async()=>{
+//     const connnn = await conn.getConnection();
+//     const [res] = await connnn.query(query);
+//     connnn.release();
+//     return res;
+// }
+const result = await queryRead(query);
 
-console.log(await test());
+
+console.log(result);
 
 
 // console.log(res);
